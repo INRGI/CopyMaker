3e2858cb18bf437889639c47d6c372813caa9116
Ruslan <fchcgcgvv@gmail.com>
2024-03-23 21:32:35 +0100

    Add redux to project

---
+++ src/redux/domainSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const domainsInitialState = [
+  { id: 'id-1', name: 'NewDomain', fontSize: '18px', fontFamily: "Tahoma" },
+];
+
+const domainSlice = createSlice({
+  name: 'domains',
+  initialState: domainsInitialState,
+  reducers: {
+    addDomain: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare({name, fontSize, fontFamily}) {
+        return {
+          payload: {
+            name,
+            fontSize,
+            fontFamily,
+            id: nanoid()
+          },
+        };
+      },
+    },
+    deleteDomain(state, action) {
+      const index = state.findIndex((domain) => domain.id === action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
+export const { addDomain, deleteDomain } = domainSlice.actions;
+export const domainReducer = domainSlice.reducer;
---
+++ src/redux/selectors.js
@@ -0,0 +1,1 @@
+export const getDomains = state => state.domains;
