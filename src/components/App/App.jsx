b9289d4dd68e7ed30f8cc9c84a95583d089b5124
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-03-23 12:24:17 +0100

    App basic routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from '../Layout';
+import { lazy } from "react";
+
+const Home = lazy(() => import("../../pages/Home"));
+const NotFound = lazy(() => import("../../pages/NotFound"));
+
 const App = () => {
-    return <h1>App</h1>
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
+    )
 }
 
 export default App;
