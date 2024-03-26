eeddd3cdce20b4b59f11efb36f3b13a6df718561
Ruslan <fchcgcgvv@gmail.com>
2024-03-26 09:56:19 +0100

    DomainList styles

--- src/components/DomainList/DomainList.jsx
+++ src/components/DomainList/DomainList.jsx
@@ -1,13 +1,87 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectVisibleDomains } from "../../redux/selectors";
+import { Button, ButtonEdit, ButtonsContainer, Container, Domain } from "./DomainList.styled";
+import { useState } from "react";
+import { Bounce, toast } from "react-toastify";
 
 const DomainList = () => {
-    
     const visibleDomains = useSelector(selectVisibleDomains);
-    
-        return visibleDomains.map(domain => (
-            <h3 key={domain.id}>{domain.name}</h3>
-        ))
+    // const dispatch = useDispatch();
+    // const [isModalOpen, setIsModalOpen] = useState(false);
+    // const [domainToDelete, setDomainToDelete] = useState(null);
+    // const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+    // const [domainToEdit, setDomainToEdit] = useState(null);
+
+    // const handleDeleteClick = (id) => {
+    //     setDomainToDelete(id);
+    //     setIsModalOpen(true);
+    // };
+
+    // const handleCancelDelete = () => {
+    //     setIsModalOpen(false);
+    //     setDomainToDelete(null);
+    // };
+
+    // const handleEditClick = (contact) => {
+    //     setDomainToEdit(contact);
+    //     setIsEditModalOpen(true);
+    // };
+
+    // const handleConfirmDelete = () => {
+    //     if (domainToDelete) {
+    //         dispatch(deleteDomain(domainToDelete));
+    //         setIsModalOpen(false);
+    //         setDomainToDelete(null);
+    //         toast.success('Domain successfully deleted', {
+    //             position: "top-right",
+    //             autoClose: 2000,
+    //             hideProgressBar: false,
+    //             closeOnClick: true,
+    //             pauseOnHover: true,
+    //             draggable: true,
+    //             progress: undefined,
+    //             theme: "light",
+    //             transition: Bounce,
+    //         });
+    //     }
+    // };
+
+    // const handleConfirmEdit = () => {
+    //     setIsEditModalOpen(false);
+    //     setDomainToEdit(null);
+    // };
+        return (
+            <Container>
+                {visibleDomains.map(domain => (
+                <Domain key={domain.id}>
+                    {domain.name}
+                    <ButtonsContainer>
+                        <ButtonEdit type='button' name='edit' onClick={() => handleEditClick(domain)}>
+                            Edit
+                        </ButtonEdit>
+                        <Button
+                            type='button'
+                            name='delete'
+                            onClick={() => handleDeleteClick(domain.id)}
+                        >
+                            Delete
+                        </Button>
+                    </ButtonsContainer>
+                </Domain>
+            ))}
+            {/* <DeleteConfirmationModal
+                isOpen={isModalOpen}
+                onClose={handleCancelDelete}
+                onConfirm={handleConfirmDelete}
+            />
+            <EditModal
+                isOpen={isEditModalOpen}
+                onClose={() => setIsEditModalOpen(false)}
+                onConfirm={handleConfirmEdit}
+                contact={domainToEdit}
+            /> */}
+        </Container>
+        );
 }
 
 export default DomainList;
---
+++ src/components/DomainList/DomainList.styled.jsx
@@ -0,0 +1,117 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.ul`
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    width: 90%;
+    padding: 0;
+`;
+
+export const Button = styled.button`
+    align-items: center;
+  appearance: none;
+  background-color: #e63946;
+  border-radius: 4px;
+  border-width: 0;
+  box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#b22b36 0 -3px 0 inset;
+  box-sizing: border-box;
+  color: white;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: "JetBrains Mono",monospace;
+  height: 48px;
+  justify-content: center;
+  line-height: 1;
+  list-style: none;
+  overflow: hidden;
+  padding-left: 16px;
+  padding-right: 16px;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  transition: box-shadow .15s,transform .15s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  will-change: box-shadow,transform;
+  font-size: 18px;
+  &:focus {
+  box-shadow: #b22b36 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #b22b36 0 -3px 0 inset;
+    }
+
+    &:hover {
+    box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #b22b36 0 -3px 0 inset;
+    transform: translateY(-2px);
+    }
+
+    &:active {
+    box-shadow: #b22b36 0 3px 7px inset;
+    transform: translateY(2px);
+    }
+`;
+
+export const Domain = styled.li`
+    display: flex;
+    justify-content: space-between;
+    font-size: 20px;
+    align-items: center;
+    border: 1px solid;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    padding: 9px;
+    border-radius: 5px;
+    max-width: 800px;
+    width: 95%;
+    margin-left:5px;
+`;
+
+export const ButtonsContainer = styled.div`
+    display: flex;
+    gap: 10px;
+`
+
+export const ButtonEdit = styled.button`
+    align-items: center;
+  appearance: none;
+  background-color: #fca311;
+  border-radius: 4px;
+  border-width: 0;
+  box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#e19110 0 -3px 0 inset;
+  box-sizing: border-box;
+  color: white;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: "JetBrains Mono",monospace;
+  height: 48px;
+  justify-content: center;
+  line-height: 1;
+  list-style: none;
+  overflow: hidden;
+  padding-left: 16px;
+  padding-right: 16px;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  transition: box-shadow .15s,transform .15s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  will-change: box-shadow,transform;
+  font-size: 18px;
+  &:focus {
+  box-shadow: #e19110 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #e19110 0 -3px 0 inset;
+    }
+
+    &:hover {
+    box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #e19110 0 -3px 0 inset;
+    transform: translateY(-2px);
+    }
+
+    &:active {
+    box-shadow: #e19110 0 3px 7px inset;
+    transform: translateY(2px);
+    }
+`;
