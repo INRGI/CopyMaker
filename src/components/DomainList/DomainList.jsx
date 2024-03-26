ca854077e8b1f452c6f85e7a2d111486a2d074b7
Ruslan <fchcgcgvv@gmail.com>
2024-03-26 10:05:12 +0100

    Delete domain option

--- src/components/DomainList/DomainList.jsx
+++ src/components/DomainList/DomainList.jsx
@@ -1,50 +1,52 @@
 import { useDispatch, useSelector } from "react-redux";
 import { selectVisibleDomains } from "../../redux/selectors";
 import { Button, ButtonEdit, ButtonsContainer, Container, Domain } from "./DomainList.styled";
+import DeleteConfirmationModal from '../DeleteConfirmationModal';
 import { useState } from "react";
 import { Bounce, toast } from "react-toastify";
+import { deleteDomain } from '../../redux/domainSlice';
 
 const DomainList = () => {
     const visibleDomains = useSelector(selectVisibleDomains);
-    // const dispatch = useDispatch();
-    // const [isModalOpen, setIsModalOpen] = useState(false);
-    // const [domainToDelete, setDomainToDelete] = useState(null);
+    const dispatch = useDispatch();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [domainToDelete, setDomainToDelete] = useState(null);
     // const [isEditModalOpen, setIsEditModalOpen] = useState(false);
     // const [domainToEdit, setDomainToEdit] = useState(null);
 
-    // const handleDeleteClick = (id) => {
-    //     setDomainToDelete(id);
-    //     setIsModalOpen(true);
-    // };
+    const handleDeleteClick = (id) => {
+        setDomainToDelete(id);
+        setIsModalOpen(true);
+    };
 
-    // const handleCancelDelete = () => {
-    //     setIsModalOpen(false);
-    //     setDomainToDelete(null);
-    // };
+    const handleCancelDelete = () => {
+        setIsModalOpen(false);
+        setDomainToDelete(null);
+    };
 
     // const handleEditClick = (contact) => {
     //     setDomainToEdit(contact);
     //     setIsEditModalOpen(true);
     // };
 
-    // const handleConfirmDelete = () => {
-    //     if (domainToDelete) {
-    //         dispatch(deleteDomain(domainToDelete));
-    //         setIsModalOpen(false);
-    //         setDomainToDelete(null);
-    //         toast.success('Domain successfully deleted', {
-    //             position: "top-right",
-    //             autoClose: 2000,
-    //             hideProgressBar: false,
-    //             closeOnClick: true,
-    //             pauseOnHover: true,
-    //             draggable: true,
-    //             progress: undefined,
-    //             theme: "light",
-    //             transition: Bounce,
-    //         });
-    //     }
-    // };
+    const handleConfirmDelete = () => {
+        if (domainToDelete) {
+            dispatch(deleteDomain(domainToDelete));
+            setIsModalOpen(false);
+            setDomainToDelete(null);
+            toast.success('Domain successfully deleted', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                transition: Bounce,
+            });
+        }
+    };
 
     // const handleConfirmEdit = () => {
     //     setIsEditModalOpen(false);
@@ -56,7 +58,9 @@ const DomainList = () => {
                 <Domain key={domain.id}>
                     {domain.name}
                     <ButtonsContainer>
-                        <ButtonEdit type='button' name='edit' onClick={() => handleEditClick(domain)}>
+                        <ButtonEdit type='button' name='edit' 
+                        // onClick={() => handleEditClick(domain)}
+                        >
                             Edit
                         </ButtonEdit>
                         <Button
@@ -69,12 +73,12 @@ const DomainList = () => {
                     </ButtonsContainer>
                 </Domain>
             ))}
-            {/* <DeleteConfirmationModal
+            <DeleteConfirmationModal
                 isOpen={isModalOpen}
                 onClose={handleCancelDelete}
                 onConfirm={handleConfirmDelete}
             />
-            <EditModal
+            {/* <EditModal
                 isOpen={isEditModalOpen}
                 onClose={() => setIsEditModalOpen(false)}
                 onConfirm={handleConfirmEdit}
