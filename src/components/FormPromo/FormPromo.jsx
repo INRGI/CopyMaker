c983247223ad7ef00916085d4dd127c85038c065
Ruslan <fchcgcgvv@gmail.com>
2024-03-30 11:49:45 +0100

    Add formik

--- src/components/FormPromo/FormPromo.jsx
+++ src/components/FormPromo/FormPromo.jsx
@@ -1,33 +1,68 @@
 
 import { useState } from "react";
-
+import { Formik, Form } from 'formik';
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { nanoid } from "nanoid";
 
 const FormPromo = () => {
     const [isFontSize, setFontSize] = useState(true);
     const [isFontFamily, setFontFamily] = useState(true);
+    const [isSubmitted, setIsSubmitted] = useState(false);
 
+
+    const fontSizeId = nanoid();
+    const fontFamilyId = nanoid();
+
+    const { domainId } = useParams();
+    const domain = useSelector(state => state.domains.find(domain => domain.id === domainId));
+
+    const initialValues = domain;
+
+    const handleSubmit = () => {
+
+        setIsSubmitted(true)
+    };
 
     return (
-        <>  
-            <div>
-                <label>
-                    <input type="checkbox" checked={isFontSize} onChange={() => setFontSize((prev) => !prev)}/>
-                    Font Size
-                </label>
+        <Formik
+            onSubmit={handleSubmit}
+            initialValues={initialValues}
+            validateOnBlur={false}
+            validateOnChange={false}
+        >
+            <Form>
+                <div>
+                    <label>
+                        <input type="checkbox" checked={isFontSize} onChange={() => setFontSize((prev) => !prev)}/>
+                        Font Size
+                    </label>
 
-                <input type="text" disabled={!isFontSize}/>
-            </div>
+                    <input id={fontSizeId} name="fontSize" type="text" disabled={!isFontSize}/>
+                </div>
 
-            <div>
-                <label>
-                    <input type="checkbox" checked={isFontFamily} onChange={() => setFontFamily((prev) => !prev)}/>
-                    Font Family
-                </label>
+                <div>
+                    <label>
+                        <input type="checkbox" checked={isFontFamily} onChange={() => setFontFamily((prev) => !prev)}/>
+                        Font Family
+                    </label>
 
-                <input type="text" disabled={!isFontFamily}/>
-            </div>
-            
-        </>
+                    <input id={fontFamilyId} name="fontFamily" type="text" disabled={!isFontFamily}/>
+                </div>
+                
+                <div>
+                    <input type="text" placeholder="Paste your copy here :)" />
+                    <button type="submit">Submit</button>
+                </div>
+
+                {isSubmitted && (
+                    <div>
+                        <p>Here will be output</p>
+                    </div>
+                )}
+                
+                </Form>  
+        </Formik>
     )
 }
 
