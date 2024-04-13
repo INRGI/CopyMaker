bc064e75fb2301d8fac8415d7ccfca9237b80426
Ruslan <fchcgcgvv@gmail.com>
2024-04-13 20:00:10 +0200

    Return button

--- src/pages/DomainPage/DomainPage.jsx
+++ src/pages/DomainPage/DomainPage.jsx
@@ -1,19 +1,28 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import FormPromo from "../../components/FormPromo";
 import { Helmet } from "react-helmet";
 import { useSelector } from "react-redux";
+import { useRef } from "react";
+import { Btn, StyledLink } from "./DomainPage.styled";
 
 const DomainPage = () => {
     const { domainId } = useParams();
     const domains = useSelector(state => state.domains);
     const domain = domains.find(domain => domain.id === domainId);
     const domainName = domain ? domain.name : "Loading...";
+    const location = useLocation();
+    const backLinkHref = useRef(location.state?.from || "/");
 
     return (
         <>
             <Helmet>
                 <title>{domainName}</title>
             </Helmet>
+
+            <StyledLink to={backLinkHref.current}>
+                <Btn type="button">Back</Btn>
+            </StyledLink>
+
             <FormPromo />
         </>
     )
