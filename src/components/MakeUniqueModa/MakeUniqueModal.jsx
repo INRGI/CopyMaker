305491e72075b3a3ecb77f6f6264cdc65c4113de
Ruslan <fchcgcgvv@gmail.com>
2024-04-26 09:59:18 +0200

    Anti spam works but need to update styles

--- src/components/MakeUniqueModa/MakeUniqueModal.jsx
+++ src/components/MakeUniqueModa/MakeUniqueModal.jsx
@@ -3,19 +3,21 @@ import { Formik, Form } from 'formik';
 
 import { Bounce, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { BtnBack, Container, Input, Title } from './MakeUniqueModal.styled';
-
+import { BtnBack, Container, CopyButton, Input, ResultContainer, ResultText, SubmitButtonDownload, Title, TitleContainer } from './MakeUniqueModal.styled';
+import { IoMdClose } from "react-icons/io";
+import { useState } from 'react';
+import makeUnique from '../../helpers/makeUnique';
 
 const MakeUniqueModal = ({ isOpen, onClose }) =>{
+    const [submitedResult, setSubmitedResult] = useState("");
 
     const initialValues = {
         textToChange: "",       
     };
 
-
-    const handleSubmit = () => {
-        
-        toast.success('', {
+    const handleSubmit = ({textToChange}) => {
+        setSubmitedResult(makeUnique(textToChange))
+        toast.success('Your text changed', {
             position: "top-right",
             autoClose: 2000,
             hideProgressBar: false,
@@ -28,16 +30,22 @@ const MakeUniqueModal = ({ isOpen, onClose }) =>{
         });
     };
 
+    const handleClose = () => {
+        setSubmitedResult("");
+        onClose();
+    };
 
     return (
         <Container
             ariaHideApp={false}
             isOpen={isOpen}
-            onRequestClose={onClose}
+            onRequestClose={handleClose}
             contentLabel="Edit Confirmation Modal"
         >
-            <Title>Anti Spam v1.0</Title>
-            <BtnBack type="button" onClick={onClose}>Back</BtnBack>
+            <TitleContainer><Title>Anti Spam v1.0</Title>
+                <BtnBack type="button" onClick={onClose}><IoMdClose size={26}/></BtnBack>
+                
+            </TitleContainer> 
             <Formik
                 initialValues={initialValues}
                 onSubmit={handleSubmit}
@@ -45,8 +53,18 @@ const MakeUniqueModal = ({ isOpen, onClose }) =>{
                 validateOnChange={false}
             >
                 <Form>
-                    <Input name="textToChange" type="text" placeholder="Your text to transform paste here" />
+                    <Input name="textToChange" type="text" placeholder="Your text to transform paste here" required/>
+                    
+                    <SubmitButtonDownload type="submit">Submit</SubmitButtonDownload>
 
+                    {submitedResult && (
+                        <div>
+                            <ResultContainer>
+                                <ResultText>{submitedResult}</ResultText>
+                                <CopyButton onClick={() => {navigator.clipboard.writeText(submitedResult)}} type="button">Copy</CopyButton>
+                            </ResultContainer>
+                        </div>
+                    )}
                 </Form>
             </Formik>
         </Container>
--- src/components/MakeUniqueModa/MakeUniqueModal.styled.jsx
+++ src/components/MakeUniqueModa/MakeUniqueModal.styled.jsx
@@ -6,7 +6,7 @@ import Modal from 'react-modal';
 export const Container = styled(Modal)`
     width: 100%;
     max-width: 350px;
-    height: 220px;
+    height: 250px;
     background-color: white;
     border: 1px solid;
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
@@ -44,11 +44,18 @@ export const Input = styled(Field)`
         color: black;
     }
     &:hover{
-        transform: translateY(-1px);
-        box-shadow: #8ecae6 0 4px 12px;
+        box-shadow: rgba(0, 0, 0, 0.25) 0px 5px 10px;
     }
 `;
 
+export const TitleContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    max-width: 300px;
+   
+`;
 
 export const BtnBack = styled.button`
     align-items: center;
@@ -62,13 +69,13 @@ export const BtnBack = styled.button`
   cursor: pointer;
   display: inline-flex;
   font-family: "JetBrains Mono",monospace;
-  height: 48px;
+  height: 30px;
   justify-content: center;
   line-height: 1;
   list-style: none;
   overflow: hidden;
-  padding-left: 16px;
-  padding-right: 16px;
+  padding-left: 4px;
+  padding-right: 4px;
   position: relative;
   text-align: left;
   text-decoration: none;
@@ -79,7 +86,6 @@ export const BtnBack = styled.button`
   white-space: nowrap;
   will-change: box-shadow,transform;
   font-size: 18px;
-  margin: 10px;
   &:focus {
   box-shadow: #b22b36 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #b22b36 0 -3px 0 inset;
     }
@@ -94,4 +100,110 @@ export const BtnBack = styled.button`
     transform: translateY(2px);
     }
   
+`;
+
+
+export const ResultText = styled.p`
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    width: 250px;
+`;
+
+export const ResultContainer = styled.div`
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 15px;
+`;
+
+
+export const CopyButton = styled.button`
+    align-items: center;
+    appearance: none;
+    background-color: #a7c957;
+    border-radius: 4px;
+    border-width: 0;
+    box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#8aa649 0 -3px 0 inset;
+    box-sizing: border-box;
+    color: white;
+    cursor: pointer;
+    display: inline-flex;
+    font-family: "JetBrains Mono",monospace;
+    height: 35px;
+    justify-content: center;
+    line-height: 1;
+    list-style: none;
+    overflow: hidden;
+    padding: 5px 10px;
+    position: relative;
+    text-align: left;
+    text-decoration: none;
+    transition: box-shadow .15s,transform .15s;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    white-space: nowrap;
+    will-change: box-shadow,transform;
+    font-size: 18px;
+    &:focus {
+    box-shadow: #8aa649 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #8aa649 0 -3px 0 inset;
+    }
+
+    &:hover {
+    box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #8aa649 0 -3px 0 inset;
+    transform: translateY(-2px);
+    }
+
+    &:active {
+    box-shadow: #8aa649 0 3px 7px inset;
+    transform: translateY(1px);
+    }
+`;
+
+
+export const SubmitButtonDownload = styled.button`
+    align-items: center;
+    appearance: none;
+    background-color: #a7c957;
+    border-radius: 4px;
+    border-width: 0;
+    box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#8aa649 0 -3px 0 inset;
+    box-sizing: border-box;
+    color: white;
+    cursor: pointer;
+    display: inline-flex;
+    font-family: "JetBrains Mono",monospace;
+    height: 35px;
+    justify-content: center;
+    line-height: 1;
+    list-style: none;
+    overflow: hidden;
+    padding-left: 16px;
+    padding-right: 16px;
+    position: relative;
+    text-align: left;
+    text-decoration: none;
+    transition: box-shadow .15s,transform .15s;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    white-space: nowrap;
+    will-change: box-shadow,transform;
+    font-size: 18px;
+    width: 100%;
+    &:focus {
+    box-shadow: #8aa649 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #8aa649 0 -3px 0 inset;
+    }
+
+    &:hover {
+    box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #8aa649 0 -3px 0 inset;
+    transform: translateY(-2px);
+    }
+
+    &:active {
+    box-shadow: #8aa649 0 3px 7px inset;
+    transform: translateY(1px);
+    }
 `;
