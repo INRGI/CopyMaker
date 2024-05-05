96391c03a7b6e44e7ebc190ea4fe666e11f59a16
Ruslan <fchcgcgvv@gmail.com>
2024-05-05 10:44:39 +0200

    Update styles

--- src/components/LinkBuilder/LinkBuilder.jsx
+++ src/components/LinkBuilder/LinkBuilder.jsx
@@ -1,10 +1,21 @@
-import { Container } from "./LinkBuilder.styled";
+import { ErrorMessage, Formik } from "formik";
+import { CLoseModal, Container, FormContainer, Input, SubmitButton } from "./LinkBuilder.styled";
+import Error from "../Error";
 
 const LinkBuilder = ({isOpen, onClose, onConfirm, builder}) => {
 
     const handleSubmit = () => {
         onConfirm()
     }
+
+    const initialValues = {
+        track: "",
+        linkId: "",
+        merge: "",
+        mark: "",
+        type: "",
+        copy: "",        
+    };
 
     return (
         <Container
@@ -13,11 +24,39 @@ const LinkBuilder = ({isOpen, onClose, onConfirm, builder}) => {
             onRequestClose={onClose}
             contentLabel="Edit Confirmation Modal"
         >   
-            <button type="button" onClick={onClose}>Close</button>
-            <h1>LinkBuilder</h1>
-            <p>{builder}</p>
+            <CLoseModal type="button" onClick={onClose}>Close</CLoseModal>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={handleSubmit}
+                validateOnBlur={false}
+                validateOnChange={false}
+            >
+                <FormContainer>
+                    <Input name="track" type="text" placeholder="Tracking domain" autoComplete="off" required/>
+                    <ErrorMessage name="track">{msg => <Error msg={msg} />}</ErrorMessage>
+
+                    <Input name="linkId" type="text" placeholder="Redirect ID / Link ID" autoComplete="off" required/>
+                    <ErrorMessage name="linkId">{msg => <Error msg={msg} />}</ErrorMessage>
+
+                    <Input name="merge" type="text" placeholder="Email Merge Tag" autoComplete="off" required/>
+                    <ErrorMessage name="merge">{msg => <Error msg={msg} />}</ErrorMessage>
+
+                    <Input name="mark" type="text" placeholder="Code Mark" autoComplete="off" required/>
+                    <ErrorMessage name="mark">{msg => <Error msg={msg} />}</ErrorMessage>
+
+                    <Input name="track" type="type" placeholder="Type of email" autoComplete="off" required/>
+                    <ErrorMessage name="type">{msg => <Error msg={msg} />}</ErrorMessage>
+
+                    <Input name="track" type="copy" placeholder="Copy Code" autoComplete="off" required/>
+                    <ErrorMessage name="copy">{msg => <Error msg={msg} />}</ErrorMessage>
+
+                    <SubmitButton type="submit">Submit</SubmitButton>
+                </FormContainer>
+            </Formik>
         </Container>
     )
 };
 
 export default LinkBuilder;
+
+// 1 – Трекінг Домен  2 – Redirect ID / Link ID 3 – Email Merge Tag 4 – код марк 5 – вид емейл 6 – абревіатура копії
--- src/components/LinkBuilder/LinkBuilder.styled.jsx
+++ src/components/LinkBuilder/LinkBuilder.styled.jsx
@@ -1,11 +1,12 @@
 import styled from "@emotion/styled";
+import { Field, Form } from "formik";
 import Modal from 'react-modal';
 
 
 export const Container = styled(Modal)`
     width: 100%;
     max-width: 350px;
-    height: 220px;
+    height: 520px;
     background-color: white;
     border: 1px solid;
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
@@ -19,4 +20,115 @@ export const Container = styled(Modal)`
     flex-direction: column;
     gap: 20px;
     align-items: center;
+`;
+
+export const FormContainer = styled(Form)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+`;
+
+export const Input = styled(Field)`
+    display: block;
+    height: 50px;
+    width: 276px;
+    background-color: rgba(255,255,255,0.07);
+    border-radius: 3px;
+    border: 1px solid;
+    padding: 0 10px;
+    font-size: 14px;
+    font-weight: 300;
+    &::placeholder{
+        color: black;
+    }
+`;
+
+
+export const SubmitButton = styled.button`
+    align-items: center;
+    appearance: none;
+    background-color: #a7c957;
+    border-radius: 4px;
+    border-width: 0;
+    box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#8aa649 0 -3px 0 inset;
+    box-sizing: border-box;
+    color: white;
+    cursor: pointer;
+    display: inline-flex;
+    font-family: "JetBrains Mono",monospace;
+    height: 35px;
+    justify-content: center;
+    line-height: 1;
+    list-style: none;
+    overflow: hidden;
+    padding-left: 16px;
+    padding-right: 16px;
+    position: relative;
+    text-align: left;
+    text-decoration: none;
+    transition: box-shadow .15s,transform .15s;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    white-space: nowrap;
+    will-change: box-shadow,transform;
+    font-size: 18px;
+    width: 150px;
+    &:focus {
+    box-shadow: #8aa649 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #8aa649 0 -3px 0 inset;
+    }
+
+    &:hover {
+    box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #8aa649 0 -3px 0 inset;
+    transform: translateY(-1px);
+    }
+    `;
+
+
+export const CLoseModal = styled.button`
+  align-items: center;
+  appearance: none;
+  background-color: #e63946;
+  border-radius: 4px;
+  border-width: 0;
+  box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#b22b36 0 -3px 0 inset;
+  box-sizing: border-box;
+  color: white;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: "JetBrains Mono",monospace;
+  height: 48px;
+  justify-content: center;
+  line-height: 1;
+  list-style: none;
+  overflow: hidden;
+  padding-left: 16px;
+  padding-right: 16px;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  transition: box-shadow .15s,transform .15s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  will-change: box-shadow,transform;
+  font-size: 18px;
+  margin: 10px;
+  &:focus {
+  box-shadow: #b22b36 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #b22b36 0 -3px 0 inset;
+    }
+
+    &:hover {
+    box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #b22b36 0 -3px 0 inset;
+    transform: translateY(-1px);
+    }
+
+    &:active {
+    box-shadow: #b22b36 0 3px 7px inset;
+    transform: translateY(1px);
+    }
+  
 `;
