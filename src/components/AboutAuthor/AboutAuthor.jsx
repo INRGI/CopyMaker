4af231495843b3ca4c7f9256f3baa1a6c2234856
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-05-23 09:39:55 +0200

    Custom Hiidden Block

--- src/components/AboutAuthor/AboutAuthor.jsx
+++ src/components/AboutAuthor/AboutAuthor.jsx
@@ -4,7 +4,7 @@ import { Container, Text } from "./AboutAuthor.styled";
 const AboutAuthor = () => {
     return (
         <Container>
-            <Text>About Author</Text>
+            <Text>Author</Text>
             <SocialIcon target="_blank" network="github" url="https://github.com/INRGI" style={{ height: 35, width: 35 }}/>
             <SocialIcon target="_blank" network="telegram" url="https://t.me/INickJackI" style={{ height: 35, width: 35 }}/>
         </Container>
---
+++ src/helpers/customHiddenBlock.js
@@ -0,0 +1,24 @@
+const addHiddenBlock = (text, symbols) => {
+
+    const generateRandomString = () => {
+        let result = '';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        for (let i = 0; i < symbols; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    };
+
+    const uniqueBlock = generateRandomString();
+
+    const newText = `<table border="0" cellpadding="0" cellspacing="0" class="body" role="presentation" style="display:none; width: 100%" ><tbody><tr><td align="center" valign="top">${uniqueBlock}</td></tr></tbody></table>${text}<table border="0" cellpadding="0" cellspacing="0" class="body" role="presentation" style="display:none; width: 100%" ><tbody><tr><td align="center" valign="top">${uniqueBlock}</td></tr></tbody></table>`;
+
+    return newText;
+};
+
+export default addHiddenBlock;
+
+{/* <table style="display:none;" role="presentation" cellpadding="0" cellspacing="0" align="center"><tr><td align="center" style="text-align: center">
+
+                </td></tr></table> */}
