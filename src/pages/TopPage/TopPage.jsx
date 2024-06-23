db74dc89d7b403d55c255e9d9cb28106c27be7be
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-06-23 19:13:09 +0200

    TopPage create containers and back btn

--- src/pages/TopPage/TopPage.jsx
+++ src/pages/TopPage/TopPage.jsx
@@ -1,10 +1,28 @@
+import { useLocation } from "react-router-dom";
 import TopFromEmail from "../../components/TopFromEmail/TopFromEmail";
-import { TopFromContainer } from "./TopPage.styled";
+import { Btn, HeaderContainer, StyledLink, TopFromContainer } from "./TopPage.styled";
+import { useRef } from "react";
+import { Helmet } from "react-helmet";
 
 const TopPage = () => {
+    const location = useLocation();
+    const backLinkHref = useRef(location.state?.from || "/");
+
     return (
-        <>
-            <h1>TopPage</h1>
+        <>  
+            <Helmet>
+                <title>Top</title>
+            </Helmet>
+            
+            <HeaderContainer>
+            <h1>Top Resources for Your Campaigns</h1>
+
+            <StyledLink to={backLinkHref.current}>
+                <Btn type="button">Back</Btn>
+            </StyledLink>
+            </HeaderContainer>
+
+
             <TopFromContainer>
                 <TopFromEmail />
             </TopFromContainer>
--- src/pages/TopPage/TopPage.styled.jsx
+++ src/pages/TopPage/TopPage.styled.jsx
@@ -1,17 +1,78 @@
-
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
 
 export const TopFromContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    align-items: center;
-    height: 380px;
-    width: 100%;
-    max-width: 300px;
-    border-radius: 16px;
-    box-shadow: 0 30px 30px -25px rgba(65, 51, 183, 0.25);
-    background-color:#fff;
-    padding-top:20px;
-    overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+  height: 380px;
+  width: 100%;
+  max-width: 300px;
+  border-radius: 16px;
+  box-shadow: 0 30px 30px -25px rgba(65, 51, 183, 0.25);
+  background-color: #fff;
+  padding-top: 20px;
+  overflow-y: auto;
+  margin-left: 30px;
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  align-items: center;
+  padding: 0 30px;
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+export const Btn = styled.button`
+  align-items: center;
+  appearance: none;
+  background-color: #e63946;
+  border-radius: 4px;
+  border-width: 0;
+  box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,
+    rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #b22b36 0 -3px 0 inset;
+  box-sizing: border-box;
+  color: white;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: "JetBrains Mono", monospace;
+  height: 48px;
+  justify-content: center;
+  line-height: 1;
+  list-style: none;
+  overflow: hidden;
+  padding-left: 16px;
+  padding-right: 16px;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  transition: box-shadow 0.15s, transform 0.15s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  will-change: box-shadow, transform;
+  font-size: 18px;
+  margin: 10px;
+  &:focus {
+    box-shadow: #b22b36 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px,
+      rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #b22b36 0 -3px 0 inset;
+  }
+
+  &:hover {
+    box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px,
+      rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #b22b36 0 -3px 0 inset;
+    transform: translateY(-2px);
+  }
+
+  &:active {
+    box-shadow: #b22b36 0 3px 7px inset;
+    transform: translateY(2px);
+  }
 `;
