c4ce61a0c101bfec2a6d3186c1ef708f2c968d24
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-07-10 09:33:29 +0200

    Finding file in google drive

--- src/components/AutoCopies/AutoCopies.jsx
+++ src/components/AutoCopies/AutoCopies.jsx
@@ -20,6 +20,8 @@ const AutoCopies = () => {
         clientId: CLIENT_ID,
         discoveryDocs: DISCOVERY_DOCS,
         scope: SCOPES,
+        ux_mode: 'popup',
+        redirect_uri: 'https://copy-maker.vercel.app',
       }).then(() => {
         gapi.auth2.getAuthInstance().signIn();
       }).catch(err => {
@@ -31,46 +33,36 @@ const AutoCopies = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError(''); // Clear previous error message
+    setError('');
     try {
-      // Step 1: Fetch the product folder based on the product name in the entire Google Drive
+
       const productQuery = `name contains '${productName}' and mimeType = 'application/vnd.google-apps.folder'`;
-      console.log('Query for product folder:', productQuery);
-      let res = await gapi.client.drive.files.list({ q: productQuery });
-      console.log('Product folder response:', res);
+      let res = await gapi.client.drive.files.list({ q: productQuery, includeItemsFromAllDrives: true, supportsAllDrives: true });
 
       if (res.result.files.length === 0) {
         throw new Error('No product folder found with the specified name.');
       }
       const productFolderId = res.result.files[0].id;
 
-      // Step 2: Fetch the "HTML+SL" subfolder within the product folder
       const subFolderQuery = `'${productFolderId}' in parents and mimeType = 'application/vnd.google-apps.folder' and name contains 'HTML+SL'`;
-      console.log('Query for HTML+SL subfolder:', subFolderQuery);
-      const subFolderRes = await gapi.client.drive.files.list({ q: subFolderQuery });
-      console.log('HTML+SL subfolder response:', subFolderRes);
+
+      const subFolderRes = await gapi.client.drive.files.list({ q: subFolderQuery, includeItemsFromAllDrives: true, supportsAllDrives: true });
 
       if (subFolderRes.result.files.length === 0) {
         throw new Error('No "HTML+SL" subfolder found.');
       }
       const subFolderId = subFolderRes.result.files[0].id;
 
-      // Step 3: Fetch the "Lift {productNumber}" subfolder within the "HTML+SL" subfolder
       const liftFolderQuery = `'${subFolderId}' in parents and mimeType = 'application/vnd.google-apps.folder' and name contains 'Lift ${productNumber}'`;
-      console.log('Query for Lift subfolder:', liftFolderQuery);
-      const liftFolderRes = await gapi.client.drive.files.list({ q: liftFolderQuery });
-      console.log('Lift subfolder response:', liftFolderRes);
+      const liftFolderRes = await gapi.client.drive.files.list({ q: liftFolderQuery, includeItemsFromAllDrives: true, supportsAllDrives: true });
 
       if (liftFolderRes.result.files.length === 0) {
         throw new Error(`No "Lift ${productNumber}" subfolder found.`);
       }
       const liftFolderId = liftFolderRes.result.files[0].id;
 
-      // Step 4: Fetch the HTML file within the "Lift {productNumber}" subfolder
       const htmlFileQuery = `'${liftFolderId}' in parents and mimeType = 'text/html'`;
-      console.log('Query for HTML file:', htmlFileQuery);
-      const fileRes = await gapi.client.drive.files.list({ q: htmlFileQuery });
-      console.log('HTML file response:', fileRes);
+      const fileRes = await gapi.client.drive.files.list({ q: htmlFileQuery, includeItemsFromAllDrives: true, supportsAllDrives: true });
 
       if (fileRes.result.files.length === 0) {
         throw new Error('No HTML file found in the specified subfolder.');
@@ -84,7 +76,6 @@ const AutoCopies = () => {
 
       setFileContent(fileContentRes.body);
     } catch (err) {
-      console.error('Error:', err);
       setError(err.message);
     }
   };
