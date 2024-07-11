b8fd9b5bd4c5979ee85e9a3b843e313a413da787
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-07-11 09:52:29 +0200

    Auto page styles

--- src/pages/AutoPage/AutoPage.jsx
+++ src/pages/AutoPage/AutoPage.jsx
@@ -1,8 +1,36 @@
 import AutoCopies from "../../components/AutoCopies/AutoCopies";
+import { useLocation, useParams } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import { useSelector } from "react-redux";
+import { useRef } from "react";
+import { Btn, PageContainer, StyledLink } from "./AutoPage.styled";
+import { ToastContainer } from "react-toastify";
 
 const AutoPage = () => {
+    const { domainId } = useParams();
+    const domains = useSelector(state => state.domains);
+    const domain = domains.find(domain => domain.id === domainId);
+    const domainName = domain ? domain.name : "Loading...";
+    const location = useLocation();
+    const backLinkHref = useRef(location.state?.from || "/");
+
     return(
-        <AutoCopies />
+        <>
+            <Helmet>
+                <title>{domainName}</title>
+            </Helmet>
+
+            <StyledLink to={backLinkHref.current}>
+                <Btn type="button">Back</Btn>
+            </StyledLink>
+            
+                <ToastContainer />
+           
+            <PageContainer>
+                <AutoCopies />
+            </PageContainer>
+        </>
+        
     )
 }
 
