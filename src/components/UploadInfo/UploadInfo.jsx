442ab4fdb12a31d25171d878e5d62eab82891726
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-07-24 10:15:23 +0200

    Update upload info

--- src/components/UploadInfo/UploadInfo.jsx
+++ src/components/UploadInfo/UploadInfo.jsx
@@ -9,7 +9,7 @@ const UploadInfo = () => {
     return (
         <>
         <Container>
-            <Text>Last <CgMonday /> Update: 22.07(09:05)</Text>
+            <Text>Last <CgMonday /> Update: 24.07(10:05)</Text>
             <SupportText onClick={()=> setDonateOpen(true)}><BiDonateHeart /> Support Us Here</SupportText>
         </Container>
         <DonateModal
