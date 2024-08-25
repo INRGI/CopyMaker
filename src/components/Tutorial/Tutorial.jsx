10a3922d66be95864b7f533009ebd9231ad02cf9
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-08-25 17:15:53 +0200

    Tutorial structure done(now we need to record videos)

--- src/components/Tutorial/Tutorial.jsx
+++ src/components/Tutorial/Tutorial.jsx
@@ -6,8 +6,35 @@ const Tutorial = () => {
     <>
       <Container>
         <BodyContainer>
-          <VideoTitle>First Video</VideoTitle>
+          <VideoTitle>How to make a copie from start</VideoTitle>
           <ReactPlayer
+            url={"https://www.youtube.com/watch?v=jNgP6d9HraI"}
+            config={{
+              youtube: {
+                playerVars: { showinfo: 1, controls: true, },
+              },
+            }}
+          />
+           <VideoTitle>Email Filter for noobies</VideoTitle>
+           <ReactPlayer
+            url={"https://www.youtube.com/watch?v=jNgP6d9HraI"}
+            config={{
+              youtube: {
+                playerVars: { showinfo: 1, controls: true, },
+              },
+            }}
+          />
+          <VideoTitle>Import/Export domains</VideoTitle>
+           <ReactPlayer
+            url={"https://www.youtube.com/watch?v=jNgP6d9HraI"}
+            config={{
+              youtube: {
+                playerVars: { showinfo: 1, controls: true, },
+              },
+            }}
+          />
+          <VideoTitle>Additional information(how functions works)</VideoTitle>
+           <ReactPlayer
             url={"https://www.youtube.com/watch?v=jNgP6d9HraI"}
             config={{
               youtube: {
