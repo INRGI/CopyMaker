4833e50979fd5d68318d0adfac1735f77689b3ce
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-08-28 09:09:40 +0200

    Not Found handling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useParams } from "react-router-dom";
 import Layout from '../Layout';
 import { lazy } from "react";
+import { useSelector } from "react-redux";
 
 const Home = lazy(() => import("../../pages/Home"));
 const DomainPage = lazy(() => import("../../pages/DomainPage"));
@@ -10,20 +11,47 @@ const TopPage = lazy(() => import("../../pages/TopPage"));
 const AutoPage = lazy(() => import("../../pages/AutoPage"));
 const TutorialPage = lazy(() => import("../../pages/TutorialPage"));
 
+const DomainWrapper = () => {
+    const { domainId } = useParams();
+    const domain = useSelector((state) =>
+    state.domains.find((domain) => domain.id === domainId)
+  );
+
+    if (!domain) {
+        return <Navigate to="/not-found" replace />;
+    }
+
+    return <DomainPage />;
+};
+
+const AutonWrapper = () => {
+    const { domainId } = useParams();
+    const domain = useSelector((state) =>
+    state.domains.find((domain) => domain.id === domainId)
+  );
+
+    if (!domain) {
+        return <Navigate to="/not-found" replace />;
+    }
+
+    return <AutoPage />;
+};
+
 const App = () => {
 
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
-                <Route path="/:domainId" element={<DomainPage />} />
+                <Route path="/:domainId" element={<DomainWrapper />} />
 
                 <Route path="/emailFilter" element={<CsvPage />} />
                 <Route path="/top" element={<TopPage />} />
                 <Route path="/tutorial" element={<TutorialPage />} />
-                <Route path="/auto/:domainId" element={<AutoPage />} />
+                <Route path="/auto/:domainId" element={<AutonWrapper />} />
 
-                <Route path="*" element={<NotFound />} />
+                <Route path="/not-found" element={<NotFound />} />
+                <Route path="*" element={<Navigate to="/not-found" replace />} />
             </Route>
         </Routes>
     )
